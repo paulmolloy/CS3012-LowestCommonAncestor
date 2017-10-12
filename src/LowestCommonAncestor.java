fc05d45cdd0436d12e63f0235dbb9ab6c83fdac3
Paul <molloyp1@tcd.ie>
2017-10-12 15:23:32 +0100

    Added Node class and stub of Node lowestCommonAncestor(Node a, Node b)

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -1,9 +1,29 @@
-
+	
 public class LowestCommonAncestor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
 
+	
+	public static Node lowestCommonAncestor(Node a, Node b) {
+		return new Node();
+	}
+
+}
+
+class Node{
+	Node parent;
+	int value;
+	
+	public Node(int value, Node parent){
+		this.parent = parent;
+		this.value = value;
+	}
+
+	public Node() {
+		// TODO Auto-generated constructor stub
+	}
 }

1bedd3690ef9f864afbf0caf854add8b6243dafb
paulmolloy <molloyp1@tcd.ie>
2017-10-18 11:13:51 +0100

    Added BST type with put and get methods

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -7,23 +7,80 @@ public class LowestCommonAncestor {
 	}
 	
 
-	
-	public static Node lowestCommonAncestor(Node a, Node b) {
-		return new Node(0);
-	}
+//	
+//	public static BST lowestCommonAncestor(Node a, Node b) {
+//		return new Node(0);
+//	}
 
 }
-
-class Node{
-	Node parent;
-	int value;
+class BST<Key extends Comparable<Key>, Value> {
+	private Node root;
 	
-	public Node(int value, Node parent){
-		this.parent = parent;
-		this.value = value;
-	}
+	private class Node{
+		private Node parent;
+		private Key key;
+		private Value val;
+		private Node left, right;
+		private int size;
+		
+		public Node(Key key, Value value, int size){
+			this.parent = parent;
+			this.val = value;
+			this.size = size;
+		}
 
-	public Node(int value) {
-		this.value = value;
 	}
+	
+	public BST() {
+		
+	}
+	
+	public boolean isEmpty() {
+		return size() ==0;
+	}
+	public int size() {
+		return size(root);
+	}
+	
+	public int size(Node n) {
+		if ( n== null) return 0;
+		else return n.size;
+	}
+	
+    public boolean contains(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+    
+    public Value get(Key key) {
+    	return get(root, key);
+    }
+    
+    private Value get(Node x, Key key) {
+    	if(key == null) throw new IllegalArgumentException("get() on a null key");
+    	if(x == null) return null;
+    	int cmp = key.compareTo(x.key);
+    	if(cmp == 0) return x.val;
+    	else if (cmp < 0 ) return get(x.left, key);
+    	else 				return get(x.right, key);
+    }
+    
+    private void put(Key key, Value val) {
+    	if (key == null) throw new IllegalArgumentException("put() on a null key");
+    	if (val == null) {
+    		//delete(key);
+    	}
+    	root = put(root, key, val);    	
+    }
+    private Node put(Node x, Key key, Value val) {
+    	if( x == null) return new Node(key, val, 1);
+    	int cmp = key.compareTo(x.key);
+    	if(cmp < 0) x.left = put(x.left, key, val);
+    	else if(cmp > 0) x.right = put(x.right, key, val);
+    	else x.val = val;
+    	x.size = 1 + size(x.left) + size(x.left.right);
+    	return x;
+
+    }
 }
+
